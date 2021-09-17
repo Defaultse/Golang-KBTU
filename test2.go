dfa0c7491b3ab7ffd70d8e680a13eecf709e4e4b
defaultse <seytay.ernar@gmail.com>
2021-09-17 18:05:34 +0600

    exercise 8

---
+++ test2.go
@@ -0,0 +1,14 @@
+package main
+
+type Vertex struct {
+	X, Y int
+}
+
+func main() {
+	// i := 42
+	// p := &i // point to i
+	// fmt.Println(p) // read i through the pointer
+	// *p = 21
+	// fmt.Println(i)
+
+}
