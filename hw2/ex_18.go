72a3e213e562ccc95ce49203eb56c17eaa85c303
defaultse <seytay.ernar@gmail.com>
2021-09-24 21:24:51 +0600

    hw2

---
+++ hw2/ex_18.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (a IPAddr) String(ip [4]byte) string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip.String(ip))
+	}
+}
---
+++ hw2/ex_22.go
@@ -0,0 +1,16 @@
+package reader
+
+type MyReader struct{}
+
+func (m MyReader) Read(b []byte) (int, error) {
+	for i := 0; i < len(b); i++ {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
+
+// couldn't import package correctly. Done in gotour's sandbox
